5a4b8401c2ac9cb4078f5880bea4c0f92979ab69
pnmatai <priya.matai@gmail.com>
2015-05-26 10:50:30 -0700

    Create transaction.php

---
+++ dropin/transaction.php
@@ -0,0 +1,28 @@
+<?php
+require_once "lib/Braintree.php";
+
+$result = Braintree_Transaction::sale(array(
+    'amount' => '10.00',
+    'paymentMethodNonce' => 'nonce-from-the-client'
+));
+
+
+if ($result->success)
+	{
+   echo'Valid Transaction!';
+    echo "<h2>Result:</h2><br> <pre>";
+    print_r($result);
+    echo "</pre>";
+	} 
+	else
+	{
+    foreach($result->errors->deepAll() AS $error) 
+	{
+        echo($error->code . ": " . $error->message . "\n");
+        echo "<h2>Result:</h2><br> <pre>";
+        print_r($result);
+        echo "</pre>";
+    }
+	}
+	
+	?>
