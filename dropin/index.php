bf2241841eba5ba9cf772e5903785544451b2643
pnmatai <priya.matai@gmail.com>
2015-05-26 10:49:44 -0700

    Update index.php

--- dropin/index.php
+++ dropin/index.php
@@ -1,1 +1,25 @@
+<?php
+require_once "lib/Braintree.php";
+$clientToken = Braintree_ClientToken::generate();
+?>
 
+<!DOCTYPE html>
+<html>
+
+<h1>Basic Dropin </h1>
+<body>
+
+<form id="checkout" method="post" action="transaction.php">
+  <div id="dropin"></div>
+  <input type="submit" value="Pay $10">
+</form>
+
+</body>
+</html>
+<script src="https://js.braintreegateway.com/v2/braintree.js"></script>
+<script type="text/javascript">
+    braintree.setup("<?php echo $clientToken?>", "dropin", {
+        container: 'dropin',
+		
+    });
+</script>
